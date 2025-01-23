d5027c981c70f95063053f31ea162f48c3714119
asbilim <bilimdepaullilian@gmail.com>
2025-01-23 06:16:05 +0100

    continuing this evening with connection to the backend , iron session and next auth , all the functionality related to auth need to be closed this evening

--- components/layout/auth.jsx
+++ components/layout/auth.jsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 import { Link } from "@/i18n/routing";
 import { Card } from "@/components/ui/card";
 import { ChevronLeft } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export default function AuthLayout({ children }) {
+  const t = useTranslations("ui.authLayout");
+
   return (
-    <div className="min-h-screen relative top-0 flex flex-col items-center justify-center p-4 w-full ">
+    <div className="min-h-screen relative top-0 flex flex-col items-center justify-center p-4 w-full">
       <Link
         href="/"
         className="absolute top-4 left-4 flex items-center text-sm text-gray-600 hover:text-gray-900 transition-colors">
         <ChevronLeft className="w-4 h-4 mr-1" />
-        Back to Home
+        {t("back_to_home")}
       </Link>
 
       <div className="w-full max-w-md">
@@ -22,11 +25,11 @@ export default function AuthLayout({ children }) {
             height={120}
             className="mx-auto"
           />
-          <h1 className="mt-6 text-3xl font-extrabold">Welcome to Commerce</h1>
+          <h1 className="mt-6 text-3xl font-extrabold">{t("welcome")}</h1>
         </div>
         <Card className="p-8 shadow-2xl">{children}</Card>
         <p className="mt-6 text-center text-sm text-gray-600">
-          &copy; {new Date().getFullYear()} Commerce. All rights reserved.
+          &copy; {new Date().getFullYear()} Commerce. {t("all_rights")}
         </p>
       </div>
     </div>
